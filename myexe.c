b91732400b104130703c8aae49cef0e7f4f75224
cexxo <101983134+cexxo@users.noreply.github.com>
2022-04-30 17:57:04 +0200

    added rarity

--- myexe.c
+++ myexe.c
@@ -3,8 +3,8 @@
 
 int main(){
 	int a;
-	system("python GenerativeArt1.1.py");
-	system("cd ERC721 && echo 1 > ./strg/decision.txt && ipfs add --recursive --progress ./metadata/ > ./strg/jsons.txt && ipfs add --recursive --progress ./Results/ > ./strg/images.txt");
-	system("python metadataUpdate.py");
+	system("python GenerativeArt1.2.py");
+	system("cd ERC721 && echo 1 > ./strg/decision.txt  && ipfs add --recursive --progress ./Results/ > ./strg/images.txt");
+	system("python metadataUpdate.py && cd ERC721 && ipfs add --recursive --progress ./metadata/ > ./strg/jsons.txt");
 	system("cd ERC721  && brownie run scripts/deploy.py --network mumbaiTesnnet");
 }
